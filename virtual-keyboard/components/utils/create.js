8f732460c32b7fec913a354ab67ba2f10627b8ce
JuliaAvona <94717941+JuliaAvona@users.noreply.github.com>
2023-04-28 20:15:44 -0600

    feat: utils - create.js

---
+++ virtual-keyboard/components/utils/create.js
@@ -0,0 +1,31 @@
+const createElement = (elem, classNames, child, parent, ...dataAttributes) => {
+  let component = null;
+  component = document.createElement(elem);
+  if (classNames) {
+    component.classList.add(...classNames.split(' '));
+  }
+  if (child) {
+    if (Array.isArray(child)) {
+      child.forEach((childElem) => {
+        component.append(childElem);
+      });
+    } else if (typeof child === 'string') {
+      component.innerHTML = child;
+    } else {
+      component.append(child);
+    }
+  }
+  if (parent) { parent.append(component);}
+  if (dataAttributes.length) {
+    dataAttributes.forEach(([attributeName, attributeValue]) => {
+      if (attributeValue) {
+        component.setAttribute(attributeName, attributeValue);
+      } else {
+        component.setAttribute(attributeName, '');
+      }
+    });
+  }
+  return component;
+};
+
+export default createElement;
